fc2750aa6c6243e100550bc97aa26bde4801c551
Aril Azam <30886831+arilazam@users.noreply.github.com>
2018-01-08 09:01:53 +0800

    Add files via upload

---
+++ Animated Border Effect for Divi Button Module.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+(function($) {
+
+    $('a.animated_border').prepend('<span class="border"></span><span class="top"></span><span class="right"></span><span class="bottom"></span><span class="left"></span>');
+
+})(jQuery);
+</script>
